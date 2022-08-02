e0f502eb5c5062753668b6ee4e7d3d3916619546
NOVA <jvcooray@gmail.com>
2022-08-02 07:13:43 +0530

    supervisor update

--- loki/get.sh
+++ loki/get.sh
@@ -1,5 +1,6 @@
 wget https://github.com/grafana/loki/releases/download/v2.6.1/loki-linux-amd64.zip
 unzip "loki-linux-amd64.zip"
 chmod a+x "loki-linux-amd64"
-mv loki-linux-amd64 run
+mkdir run
+mv loki-linux-amd64 run/loki-linux-amd64
 mv loki-local-config.yaml run/loki-local-config.yaml
---
+++ run.sh
@@ -0,0 +1,11 @@
+chmod 777 loki/get.sh
+cd loki
+./get.sh
+
+chmod 777 node_exporter/get.sh
+cd node_exporter
+./get.sh
+
+chmod 777 prometheus/get.sh
+cd prometheus
+./get.sh
